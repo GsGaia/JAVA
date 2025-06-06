ae4211aff22f09f51a34850ffa38425854593618
Lu1zEdu <le6269199@gmail.com>
2025-06-05 21:42:15 -0300

    Na teoria esta pronto

--- src/main/java/br/com/fiap/Gs/Gaia/Models/Accident.java
+++ src/main/java/br/com/fiap/Gs/Gaia/Models/Accident.java
@@ -2,6 +2,7 @@ package br.com.fiap.Gs.Gaia.Models;
 
 import br.com.fiap.Gs.Gaia.Enum.TypeAccident;
 import br.com.fiap.Gs.Gaia.Enum.TypeSeverity;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 
@@ -23,6 +24,7 @@ public class Accident {
 
     @OneToOne
     @JoinColumn(name = "id_location")
+    @JsonBackReference("location-accident")
     private Location location;
 
     public Accident() {
